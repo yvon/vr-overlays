d1ad0f7f89b10d4db3f313ad93fc2c3b0a1d3756
Yvon Cognard <yvon@cognard.com>
2020-06-07 19:15:11 +0200

    Create overlay from image

--- main.cpp
+++ main.cpp
@@ -1,17 +1,47 @@
 #include <QApplication>
 #include <QWebEngineView>
+#include <openvr.h>
 
-int main(int argc, char *argv[])
-{
+using namespace vr;
+
+const int width = 550;
+const int height = 300;
+
+void check_error(int line, EVRInitError error) {if (error != 0) printf("%d: error %s\n", line, VR_GetVRInitErrorAsSymbol(error)); }
+
+int main(int argc, char** argv) {
+    (void)argc; (void)argv;
+    EVRInitError error;
+    VR_Init(&error, vr::VRApplication_Overlay);
+    check_error(__LINE__, error);
+
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
 
     QWebEngineView view;
     view.setUrl(QUrl("http://localhost:8080/ui/#/chatoverlays/twitch"));
-    view.resize(550, 300);
+    view.resize(width, height);
     view.show();
 
     view.page()->setBackgroundColor(Qt::darkGray);
+
+    QImage image(width, height, QImage::Format_ARGB32);
+
+    view.render(&image);
+
+    VROverlayHandle_t handle;
+    VROverlay()->CreateOverlay("image", "image", &handle); /* key has to be unique, name doesn't matter */
+    VROverlay()->SetOverlayFromFile(handle, "C:\\Users\\Yvon\\Downloads\\sample.png");
+    VROverlay()->SetOverlayWidthInMeters(handle, 3);
+    VROverlay()->ShowOverlay(handle);
+
+    vr::HmdMatrix34_t transform = {
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f, -2.0f
+    };
+    VROverlay()->SetOverlayTransformAbsolute(handle, TrackingUniverseStanding, &transform);
+
 
     return app.exec();
 }
--- twitch-chat.pro
+++ twitch-chat.pro
@@ -1,3 +1,5 @@
 TEMPLATE = app
 QT += webenginewidgets
 SOURCES += main.cpp
+INCLUDEPATH += C:\Users\Yvon\Documents\openvr\headers
+LIBS += C:/Users/Yvon/Documents/openvr/lib/win64/openvr_api.lib
