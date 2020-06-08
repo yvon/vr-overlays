49c21be584a88a07cb117f8dfcb3c3e9e5b266ad
Yvon Cognard <yvon@cognard.com>
2020-06-08 21:08:53 +0200

    Transparency

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QWebEngineView>
 #include <QTimer>
+#include <QOpenGLTexture>
 #include <openvr.h>
 
 using namespace vr;
@@ -20,16 +21,22 @@ int main(int argc, char** argv) {
     check_error(__LINE__, error);
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication app(argc, argv);
 
     QWebEngineView view;
+
+    // view.page()->setBackgroundColor(Qt::darkGray);
+    // view.page()->setBackgroundColor(Qt::transparent);
+    view.setAttribute(Qt::WA_TranslucentBackground);
+    view.setStyleSheet("background:transparent");
+    // https://bugreports.qt.io/browse/QTBUG-41960
+    view.page()->setBackgroundColor(Qt::transparent);
 
     pView = &view;
     view.setUrl(QUrl("http://localhost:8080/ui/#/chatoverlays/twitch"));
     view.resize(width, height);
     view.show();
-
-    view.page()->setBackgroundColor(Qt::darkGray);
 
     QImage image(width, height, QImage::Format_ARGB32);
     pImage = &image;
@@ -39,14 +46,18 @@ int main(int argc, char** argv) {
 
         // https://stackoverflow.com/questions/55231170/taking-a-screenshot-of-a-web-page-in-pyqt5
         pView->grab().save("test.png", "PNG");
-        //self.grab().save(self.output_file, b'PNG')
 
         pView->render(pImage);
+
+        QOpenGLTexture texture(*pImage);
 
         VROverlayHandle_t handle;
         VROverlay()->CreateOverlay("image", "image", &handle); /* key has to be unique, name doesn't matter */
         //VROverlay()->SetOverlayRaw(handle, pImage->bits(), width, height, 32);
         VROverlay()->SetOverlayFromFile(handle, "C:\\Users\\Yvon\\Documents\\build-twitch-chat-Desktop_Qt_5_15_0_MSVC2019_64bit-Debug\\test.png");
+        //VROverlay()->SetOverlayFromFile(handle, "C:\\Users\\Yvon\\Downloads\\test.png");
+        //vr::Texture_t myTexture = {(void*)(uintptr_t)texture.textureId(), vr::TextureType_OpenGL, vr::ColorSpace_Auto };
+        //VROverlay()->SetOverlayTexture(handle, &myTexture);
         VROverlay()->SetOverlayWidthInMeters(handle, 3);
         VROverlay()->ShowOverlay(handle);
 
