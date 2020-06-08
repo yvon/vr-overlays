000870999f34c2880c373b2414c4d0c73e254ded
Yvon Cognard <yvon@cognard.com>
2020-06-08 23:38:12 +0200

    Cleanup

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <QTimer>
 #include <openvr.h>
 #include <QOpenGLTexture>
-#include <QOpenGLFunctions>
 
 using namespace vr;
 
@@ -12,8 +11,8 @@ const int height = 300;
 
 void check_error(int line, EVRInitError error) {if (error != 0) printf("%d: error %s\n", line, VR_GetVRInitErrorAsSymbol(error)); }
 
-QWebEngineView *pView;
-VROverlayHandle_t *pHandle;
+QWebEngineView *view;
+VROverlayHandle_t *handle;
 
 int main(int argc, char** argv) {
     (void)argc; (void)argv;
@@ -25,52 +24,41 @@ int main(int argc, char** argv) {
 
     QApplication app(argc, argv);
 
-    QWebEngineView view;
+    view = new QWebEngineView();
 
     // Transparency
     // https://stackoverflow.com/questions/28183738/transparent-background-in-qwebenginepage
-    view.setAttribute(Qt::WA_TranslucentBackground);
-    view.setStyleSheet("background:transparent");
-    view.page()->setBackgroundColor(Qt::transparent);
+    view->setAttribute(Qt::WA_TranslucentBackground);
+    view->setStyleSheet("background:transparent");
+    view->page()->setBackgroundColor(Qt::transparent);
 
-    pView = &view;
+    // Load page
+    view->setUrl(QUrl("http://localhost:8080/ui/#/chatoverlays/twitch"));
+    view->resize(width, height);
+    view->show();
 
-    view.setUrl(QUrl("http://localhost:8080/ui/#/chatoverlays/twitch"));
-    view.resize(width, height);
-    view.show();
+    // Create Overlay
+    handle = new VROverlayHandle_t();
 
-    // Overlay
-    VROverlayHandle_t handle;
-    pHandle = &handle;
-
-    VROverlay()->CreateOverlay("image", "image", &handle); /* key has to be unique, name doesn't matter */
-    VROverlay()->SetOverlayWidthInMeters(handle, 3);
-    VROverlay()->ShowOverlay(handle);
+    VROverlay()->CreateOverlay("image", "image", handle); /* key has to be unique, name doesn't matter */
+    VROverlay()->SetOverlayWidthInMeters(*handle, 3);
+    VROverlay()->ShowOverlay(*handle);
 
     vr::HmdMatrix34_t transform = {
         1.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f, 1.0f,
         0.0f, 0.0f, 1.0f, -2.0f
     };
-    VROverlay()->SetOverlayTransformAbsolute(handle, TrackingUniverseStanding, &transform);
+    VROverlay()->SetOverlayTransformAbsolute(*handle, TrackingUniverseStanding, &transform);
 
     QTimer timer;
-    timer.setInterval(1000);
+    timer.setInterval(1000 / 30); // Apprx. 30FPS
     timer.callOnTimeout([] {
-        qDebug("Overlay");
-
-        //glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-        glEnable(GL_BLEND);
-
-        // https://stackoverflow.com/questions/55231170/taking-a-screenshot-of-a-web-page-in-pyqt5
-        pView->grab().save("test.png", "PNG");
-        QImage image = pView->grab().toImage();
+        QImage image = view->grab().toImage();
         QOpenGLTexture texture(image.mirrored());
-        qDebug() << texture.format();
         Texture_t myTexture = {(void*)(uintptr_t)texture.textureId(), TextureType_OpenGL, ColorSpace_Auto};
 
-        VROverlay()->SetOverlayTexture(*pHandle, &myTexture);
-        // VROverlay()->SetOverlayFromFile(*pHandle, "C:\\Users\\Yvon\\Documents\\build-twitch-chat-Desktop_Qt_5_15_0_MSVC2019_64bit-Debug\\test.png");
+        VROverlay()->SetOverlayTexture(*handle, &myTexture);
     });
     timer.start();
 
--- twitch-chat.pro
+++ twitch-chat.pro
@@ -2,4 +2,4 @@ TEMPLATE = app
 QT += webenginewidgets gui
 SOURCES += main.cpp
 INCLUDEPATH += C:\Users\Yvon\Documents\openvr\headers
-LIBS += C:/Users/Yvon/Documents/openvr/lib/win64/openvr_api.lib opengl32.lib
+LIBS += C:/Users/Yvon/Documents/openvr/lib/win64/openvr_api.lib
